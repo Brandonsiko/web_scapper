000156edc0b35ef99cf315066a80d99f9ddaf066
Jayson <brabdonsiko3@gmail.com>
2023-07-12 22:38:43 +0200

    made a few changes to how the data is generated and the formating of json files

--- Varsity.py
+++ Varsity.py
@@ -3,12 +3,17 @@ import requests
 import json
 
 def setup():
+    
+    """Get all Data of universities here and return the data as an html file"""
+    
     webUniNames=requests.get("https://businesstech.co.za/news/trending/101412/here-are-south-africas-26-universities/").text
     soup=BeautifulSoup(webUniNames,'lxml')
     content=soup.find_all('tr')
     return content
 
 def Data(content):
+    """This writes data into a text file which will be later used to access data recieved"""
+    
     filename="VarsityNames"
     with open(filename,'w') as uniNames:
         for item in content:
@@ -27,8 +32,11 @@ def Initialize(filename):
         return 0
 
 def justNames(filename):
+    """This function just saves names of varsities nothing much! and filters everything else"""
+    
     myinfoFile=[]
     south_african_varsities=[]
+    
     with open(filename,'r') as myfile:
         for item in myfile.readlines():
             if item=="\n":
@@ -36,6 +44,7 @@ def justNames(filename):
             else:
                 myinfoFile.append(item)
         myfile.close()
+        
     for i in range(len(myinfoFile)):
         if i%2==0:
             south_african_varsities.append(myinfoFile[i])
@@ -44,6 +53,7 @@ def justNames(filename):
     return south_african_varsities
 
 def varistyData(list_of_names):
+    
     with open("varsityData",'w') as data_info:
         for item in list_of_names:
             data_info.writelines(item)
@@ -52,15 +62,37 @@ def varistyData(list_of_names):
 
 def createJsonFile(myList):
     """make a dictionary of names of varsities"""
+    
     listToDict={}
     value={}
     for i in range(len(myList)):
         listToDict[i]=myList[i]
-    value[0]={"namesOfVarsities":listToDict}
+    value={"Varsities":listToDict}
     return value
 
-mything=justNames("VarsityNames")
-value=(createJsonFile(mything))
-well=json.dumps(value)
-with open("Vnames.json", "w") as outfile:
-    outfile.write(well)
+def AllData(filename):
+    """All Data which will be needed by the application """
+    gk=[]
+    final_list=[]
+    sm=[]
+    with open(filename,'r') as fn: 
+        gk= fn.readlines()
+        for i in range(len(gk)):
+            sm.append(gk[i])
+            if len(sm)==2:
+                kl={"Name":sm[0]}
+                kl2={"Location":sm[1]}
+                yu={"University":[kl,kl2]}
+                final_list.append(yu)
+                sm=[]
+            
+    return final_list
+
+
+
+if "__main__"==__name__:
+    value=createJsonFile(AllData("VarsityNames"))
+    with open("Varsities.json", "w") as outfile:
+        outfile.write(json.dumps(value))
+    
+    
